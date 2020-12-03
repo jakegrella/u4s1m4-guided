33237cf911feb637d5f29ad3298e875e5a7d6032
Jake Grella <jgrella13@gmail.com>
2020-12-03 12:54:05 -0600

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
+// development
+// production
+function url(path) {
+	return process.env.NODE_ENV === 'development'
+		? `http://localhost:5000${path}`
+		: '';
+}
 
 function App() {
+	const [data, setData] = useState('');
+	useEffect(() => {
+		// https://heroku.com/nice-app
+		// http://localhost:5000
+		fetch(url('/api/data'))
+			.then((res) => res.json())
+			.then((goods) => setData(goods.data));
+	}, []);
 	return (
 		<div className='App'>
 			<header className='App-header'>
 				<p>u4s1m4</p>
+				{data}
 			</header>
 		</div>
 	);
